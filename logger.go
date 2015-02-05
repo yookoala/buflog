624db3618a14e3b14e46ffb57f83cebaf7efc3a3
Koala Yeung <koalay@gmail.com>
2015-02-06 01:42:14 +0800

    Implemented most of standard logger functions
    Implemented most of standard logger functions in Logger. Improve
    compatibility with standard logger

--- logger.go
+++ logger.go
@@ -11,9 +11,36 @@ type Logger struct {
 	logs []*L
 }
 
-// normal logger function
+// implements standard logger function
+func (lr *Logger) Print(msg string) {
+	lr.Printf(msg)
+}
+
+// implements standard logger function
 func (lr *Logger) Printf(msg string, params ...interface{}) {
 	lr.logs = append(lr.logs, NewLog(msg, params...))
+}
+
+// implements standard logger function
+func (lr *Logger) Panic(msg string) {
+	lr.Panicf(msg)
+}
+
+// implements standard logger function
+func (lr *Logger) Panicf(msg string, params ...interface{}) {
+	lr.logs = append(lr.logs, NewPanic(msg, params...))
+	lr.Play() // play back immediately
+}
+
+// implements standard logger function
+func (lr *Logger) Fatal(msg string) {
+	lr.Fatalf(msg)
+}
+
+// implements standard logger function
+func (lr *Logger) Fatalf(msg string, params ...interface{}) {
+	lr.logs = append(lr.logs, NewFatal(msg, params...))
+	lr.Play() // play back immediately
 }
 
 // playback
